bca7060dbb8705b5e5c5b954a26095690f464a3b
ipushker <ishitap2005@gmail.com>
2025-07-13 00:08:40 +0530

    Add files via upload

---
+++ ThirdLargestElement.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ThirdLargestElement {
+    static int thirdLargest(int[] arr){
+        int n=arr.length;
+        Arrays.sort(arr);
+        return arr[n-3];
+    }
+    public static void main(String [] args){
+        int [] arr={1,14,2,16,10,20};
+        System.out.println(thirdLargest(arr));
+    }
+}
+
