9b50a958f9cfb2efb7720e3ffd3c8e3a820c5e4f
ipushker <ishitap2005@gmail.com>
2025-07-13 00:37:09 +0530

    Add files via upload

---
+++ MaximumConsecutive1sInABinaryArray.java
@@ -0,0 +1,24 @@
+
+public class MaximumConsecutive1sInABinaryArray {
+    static int maxConsecBits(int [] arr){
+        if(arr.length==0){
+            return 0;
+        }
+        int maxCount=0;
+        int count=1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]==arr[i-1]){
+                count++;
+            }
+            else{
+                maxCount=Math.max(maxCount,count);
+                count=1;
+            }
+        }
+        return Math.max(maxCount, count);
+    }
+    public static void main(String[] args){
+        int[] arr={0,1,0,1,1,1,1};
+        System.out.println(maxConsecBits(arr));
+    }
+}
