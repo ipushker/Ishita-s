701cb3a26b86097d83a7c060bd302562adb90f95
ipushker <ishitap2005@gmail.com>
2025-07-14 22:37:34 +0530

    Add files via upload

---
+++ ArrayList1.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class ArrayList1 {
+    public static void main(String []args){
+        ArrayList<Integer> a=new ArrayList<Integer>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        System.out.println(a);
+    }
+}
